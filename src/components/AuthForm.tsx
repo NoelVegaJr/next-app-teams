846953b71c4875fdfd248997c4e2d0f327576b01
NoelVegaJr <noeljr126@gmail.com>
2023-03-21 17:57:38 -0400

    auth form animations

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -12,6 +12,7 @@ export default function AuthForm({ type }: { type: "signin" | "signup" }) {
     const cleanEmail = email.trim();
     if (!cleanEmail) return;
 
+    console.log("email sent to ", cleanEmail);
     signIn("email", {
       email: cleanEmail,
       redirect: false,
@@ -47,48 +48,51 @@ export default function AuthForm({ type }: { type: "signin" | "signup" }) {
         };
 
   return (
-    <motion.form
+    <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
-      onSubmit={handleSignIn}
-      className=" bg-gray-100 mx-auto p-12 rounded-sm overflow-hidden"
     >
-      <div className="mb-4">
-        <div className="">
-          <p>Codefork</p>
-        </div>
-      </div>
-      <motion.div
-        initial={{ x: 300 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 0.5 }}
+      <form
+        className=" bg-gray-100 mx-auto p-12 rounded-sm overflow-hidden"
+        onSubmit={handleSignIn}
       >
-        <div className="mb-2">
-          <p className="font-semibold text-2xl">{form.title}</p>
-          <p>to continue to codefork teams</p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="border-b border-black outline-none bg-transparent mb-4 pb-2"
-            placeholder="Email"
-          />
-
-          {form.callout}
-
-          <div className="flex justify-end">
-            <button
-              type="submit"
-              className="py-1 px-3 rounded-sm bg-slate-700 text-white semibold w-fit"
-            >
-              Submit
-            </button>
+        <div className="mb-4">
+          <div className="">
+            <p>Codefork</p>
           </div>
         </div>
-      </motion.div>
-    </motion.form>
+        <motion.div
+          initial={{ x: 300 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="mb-2">
+            <p className="font-semibold text-2xl">{form.title}</p>
+            <p>to continue to codefork teams</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border-b border-black outline-none bg-transparent mb-4 pb-2"
+              placeholder="Email"
+            />
+
+            {form.callout}
+
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="py-1 px-3 rounded-sm bg-slate-700 text-white semibold w-fit"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </motion.div>
+      </form>
+    </motion.div>
   );
 }
